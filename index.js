084f10f0b1a05fe38be60ba0ae76b2aba5acc69d
Sheryl <sheryldreyer2@gmail.com>
2023-07-06 23:27:56 +0200

    first commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const redirectIfAuthMiddleware = require('./middleware/redirectIfAuthMiddleware'
 // Connecting to the database
 const mongoose = require('mongoose');
 mongoose.connect(
-    'mongodb://127.0.0.1:27017/my_database',
+    'mongodb://localhost:27017/my_database',
     { useNewUrlParser: true }
 );
 
